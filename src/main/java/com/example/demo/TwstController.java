5a83c1ff843f9b787dfad6960c76845214083f0f
TANUJ KUMAR SAINI <41848881+tanujkumarsaini@users.noreply.github.com>
2023-09-29 16:09:18 +0530

    Update TwstController.java

--- src/main/java/com/example/demo/TwstController.java
+++ src/main/java/com/example/demo/TwstController.java
@@ -9,7 +9,7 @@ public class TwstController {
     @GetMapping
     public Object hello() {
         Map<String, String> object = new HashMap<>();
-        object.put("name", "Integration Ninjas");
+        object.put("name", "Integration Ninjas Tanuj");
         object.put("email", "integrationninjas@gmail.com");
         return object;
     }
